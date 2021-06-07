99b420f7bbd8040f5901d43e5bd25738af3702a1
revanthkumaar <iamrevanthkumar@gmail.com>
2021-06-07 12:17:56 +0530

    lsakdjlasd
    alaksjd

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import HomePage from './screens/HomePage/HomePage.component';
 
 function App() {
   return (
     <div className="App">
      <p>Hello Whats up with you?</p>
+     <HomePage/>
     </div>
   );
 }
---
+++ src/screens/HomePage/HomePage.component.js
@@ -0,0 +1,15 @@
+
+import './HomePage.css';
+
+function HomePage() {
+
+    //javascript
+
+  return (
+    <div className="home-page">
+     <p>Reply from homepage component: Hi am doing fine</p>
+    </div>
+  );
+}
+
+export default HomePage; //allow the homepage component to get imported where ever i want
