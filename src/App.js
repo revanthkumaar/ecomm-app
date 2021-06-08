68f2b85618387f732422cb7b6068c147ea421728
revanthkumaar <iamrevanthkumar@gmail.com>
2021-06-08 11:51:09 +0530

    askdhl
    jlakjldas

--- src/App.js
+++ src/App.js
@@ -1,65 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
 
-import Lifecycles from './lifecycles.component'
+import HomePage from './pages/homepage/homepage.component';
 
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE </h1>
+  </div>
+);
 
-class App extends React.Component{
-
-  constructor() {
-    super();
-
-    console.log('constructor of app component')
-
-    //data
-
-    this.state = { //INITIALIZE THE DATA
-      showChild: true,
-      text: 'initial Revanth',
-      number: 123
-    };
-  }
-
-  render(){
-    //throwing HTML to the DOM
-    console.log('render of app component')
-    return(
-      <div className='App'>
-      <header className='App=header'>
-
-        <img src={logo} className='App-logo' alt='logo'/>
-
-        <button onClick={ () => 
-
-        this.setState(state => ({
-          showChild: !state.showChild
-        }))}>
-        TOGGLE Lifecycles COMPONENT
-        </button>
-
-
-        <button
-            onClick={() =>
-              this.setState(state => ({
-                text: state.text + '_hello'
-              }))
-            }
-          >
-            Update Text
-          </button>
-
-      {this.state.showChild ?   <Lifecycles text={this.state.text} number={this.state.number} /> : null }
-
-      </header>
-
-
-      </div>
-    )
-
-  }
-
+function App() {
+  return (
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
+    </div>
+  );
 }
-
 
 export default App;
